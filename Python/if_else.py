576f65ef11965f3b886c3b897d7c98d75f79278c
yebente <yebente@gmail.com>
2025-03-30 16:17:18 +0300

    If else with else part

--- Python/if_else.py
+++ Python/if_else.py
@@ -4,3 +4,15 @@ if temperature > 37:
     print("You have a fever")
 else:
     print("You are doing great in this cold weather")
+
+
+
+bill = 1900
+
+discount = 0.5
+
+if bill > 2000:
+    print("You have a 20% discount on your"+ " " + str(bill) + " " + "bill" + " " + "You will pay" + " " + str(bill - (bill * discount)))
+
+else:
+    print("You need to spend a little more than 2000 to get a discount")
