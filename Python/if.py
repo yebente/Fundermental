69137dccb615d7c4647f3118e94512cdcc1ef496
yebente <yebente@gmail.com>
2025-03-30 15:59:40 +0300

    If statement some maths

--- Python/if.py
+++ Python/if.py
@@ -1,3 +1,8 @@
 hungry = True
 if hungry:
     print("Eat something")
+
+
+bill = 2500
+if bill >2000:
+    print("Pay the bill, you can afford a bill less than 10,000 and you have pushed it to 2500")
