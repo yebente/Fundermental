357a267a817bfca25239c3ebb8077e7b3a628592
yebente <yebente@gmail.com>
2025-03-30 16:29:30 +0300

    If elif else the else part

---
+++ Python/if_elif_else.py
@@ -0,0 +1,10 @@
+temperature = 7
+
+if temperature > 25:
+    print("Mungu ameamua lazima tuive")
+
+elif temperature > 15:
+    print("Joto la kadiri")
+
+else:
+    print("Leo lazima tuvae majacket")
