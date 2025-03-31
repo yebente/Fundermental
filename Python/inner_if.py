c20d2e403f1a2d5130855d2acfb6f43355b87e1e
yebente <yebente@gmail.com>
2025-03-31 12:09:15 +0300

    inner if

---
+++ Python/inner_if.py
@@ -0,0 +1,12 @@
+Temperature = 25
+
+#outer if
+if Temperature >= 25:
+    #inner if
+    if Temperature == 25:
+        print("Temperature is exactly room temperature")
+    else:
+        print("Temperature is above room temperature")
+
+else:
+    print("Temperature is below room temperature")
