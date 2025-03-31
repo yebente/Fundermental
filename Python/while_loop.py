8e4b6cfd36fd9d9de6d0e2211fb4fb90146d6c73
yebente <yebente@gmail.com>
2025-03-31 13:58:54 +0300

    for while loop

--- Python/while_loop.py
+++ Python/while_loop.py
@@ -3,3 +3,7 @@ count = 1
 while count <= 20:
     print (f"This is count Number: {count}")
     count += 1
+
+for i in range(1, 4):  # Outer loop
+    for j in range(1, 4):  # Inner loop
+        print(f"Outer loop: {i}, Inner loop: {j}")
