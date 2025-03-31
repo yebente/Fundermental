09f9d6b50bee2897de0b161e1af52de597ef5170
yebente <yebente@gmail.com>
2025-03-31 11:56:02 +0300

    sum of numbers in a list that are less than a number

---
+++ Python/sum_less_than.py
@@ -0,0 +1,7 @@
+# Write a function to culculate the sum of elements in a list that are less than a given number.
+
+def sum_less_than(numbers, n):
+    # getting all numbers less than n
+    return sum(num for num in numbers if num < n)
+ #call the function
+    print(sum_less_than([1, 2, 3, 4, 5, 6, 7], 5))
