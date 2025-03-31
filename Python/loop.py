8a4043731f9e8f864874fad763403f801b111d6a
yebente <yebente@gmail.com>
2025-03-31 13:56:42 +0300

    for while loop

--- Python/loop.py
+++ Python/loop.py
@@ -2,3 +2,9 @@ fruits = ['apple', 'banana', 'cherry']
 
 for x in fruits:
     print(f"I love {x}")
+
+
+instructors = ('John', 'Jane', 'Jim')
+
+for instructor in instructors:
+    print[1]
---
+++ Python/while_loop.py
@@ -0,0 +1,5 @@
+count = 1
+
+while count <= 20:
+    print (f"This is count Number: {count}")
+    count += 1
