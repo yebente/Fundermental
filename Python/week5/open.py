b0db6bff25dc3aa6f89e2ac2c278b9c140ed148f
yebente <yebente@gmail.com>
2025-04-09 10:08:42 +0300

    Proper excemption handling

--- Python/week5/open.py
+++ Python/week5/open.py
@@ -1,2 +1,10 @@
-f = open("demofile.txt", "r")
-print(f.read())
+try:
+    f = open("demofile.txt", "r")
+    print(f.read())
+except FileNotFoundError:
+    print("File not found")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    if 'f' in locals() and not f.closed:
+        f.close()
