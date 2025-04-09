a498a84a5506efc9c9ac0151f1e10444b483ad94
yebente <yebente@gmail.com>
2025-04-09 10:23:55 +0300

    Modern excemption handling using with

--- Python/week5/open.py
+++ Python/week5/open.py
@@ -8,3 +8,14 @@ except Exception as e:
 finally:
     if 'f' in locals() and not f.closed:
         f.close()
+
+
+# Modern way to open a file using 'with' statement
+try:
+    with open("demofile.txt", "r") as f:
+        print(f.read())
+except FileNotFoundError:
+    print("File not found")
+except Exception as e:
+    print(f"An error occurred: {e}")
+# The file is automatically closed after the block of code is executed.
